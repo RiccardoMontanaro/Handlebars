b31e4c21f4a00fd86792d4f83495b059ef29d0d1
Riccardo <riccardomontanaro02@gmail.com>
2020-11-14 12:51:49 +0100

    fixing 3

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const handlebars = require('express-handlebars');
 app.set('view engine', 'hbs');
 
 app.engine('hbs', handlebars({
-layoutsDir: __dirname + '/views/layouts',
-extname: 'hbs',
-defaultLayout: 'planB',
-partialsDir: __dirname + '/views/partials/'
+	layoutsDir: __dirname + '/views/layouts',
+	extname: 'hbs',
+	defaultLayout: 'planB',
+	partialsDir: __dirname + '/views/partials/'
 }));
 
 app.use(express.static('public'))
@@ -42,7 +42,7 @@ fakeApi = () => {
 }
 
 app.get('/', (req, res) => {
-res.render('main', {layout: 'index', suggestedChamps: fakeApi(), listExists: true});
+	res.render('main', {layout: 'index', suggestedChamps: fakeApi(), listExists: true});
 });
-//a
+
 app.listen(port, () => console.log(`App listening to port ${port}`));
